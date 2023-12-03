4fc375529abacc77d33e02ef1dd8919b3846f4b8
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-12-03 17:56:04 -0300

    autenticação finalizada

--- play-chat-front/src/Components/AuthPage/index.tsx
+++ play-chat-front/src/Components/AuthPage/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-import './styles.scss'
+import './styles.scss';
 
 const socket = io('http://localhost:3000');
 
@@ -14,14 +14,14 @@ interface Message {
   content: string;
 }
 
-
-const Index = () => {
+const index = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isJoined, setIsJoined] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     console.log('Client connected to WebSocket');
@@ -37,21 +37,42 @@ const Index = () => {
       ]);
     };
 
+    const handleJoinError = (errorMessage: string) => {
+      setError(errorMessage);
+    };
+
     socket.on('message', handleIncomingMessage);
     socket.on('userJoined', handleUserJoined);
+    socket.on('joinError', handleJoinError);
+
+    const handleAuthenticationSuccess = () => {
+      setIsJoined(true);
+    };
+
+    socket.on('authenticationSuccess', handleAuthenticationSuccess);
 
     return () => {
       socket.off('message', handleIncomingMessage);
       socket.off('userJoined', handleUserJoined);
+      socket.off('joinError', handleJoinError);
+      socket.off('authenticationSuccess', handleAuthenticationSuccess);
     };
+    
   }, []);
 
-  const handleJoin = (event: React.FormEvent) => {
+  const handleJoin = async (event: React.FormEvent) => {
+    console.log("HandleJoin executado!!")
     event.preventDefault();
+    setError(null);
 
     if (name.trim() !== '' && email.trim() !== '' && password.trim() !== '') {
-      setIsJoined(true)
-      socket.emit('join', { name, email, password });
+      socket.emit('join', { name, email, password }, (response: { error: string } | null) => {
+        if (response && response.error) {
+          setError(response.error);
+        } else {
+          setIsJoined(true);
+        }
+      });
     }
   };
 
@@ -68,10 +89,11 @@ const Index = () => {
       {!isJoined ? (
         <form className="authInputsContainer" onSubmit={handleJoin}>
           <h1>Preencha os dados para entrar no chat</h1>
-          <input placeholder="Nome..." type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+          <input placeholder="Nome de usuário..." type="text" value={name} onChange={(e) => setName(e.target.value)} required />
           <input type="email" placeholder="E-mail..." value={email} onChange={(e) => setEmail(e.target.value)} required />
           <input type="password" placeholder="Senha..." value={password} onChange={(e) => setPassword(e.target.value)} required />
           <button type="submit">Entrar no Chat</button>
+          {error && <p className="error-message">{error}</p>}
         </form>
       ) : (
         <div className="chatContainer">
@@ -104,4 +126,4 @@ const Index = () => {
   );
 };
 
-export default Index;
+export default index;
