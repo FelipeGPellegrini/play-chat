bf0d683c21d42b9ec09b0b23fba1648770780031
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-12-06 14:30:43 -0300

    CORS

--- play-chat-back/src/main.ts
+++ play-chat-back/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: '*',
+    methods: 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+  });
+
   await app.listen(3000);
 }
 
